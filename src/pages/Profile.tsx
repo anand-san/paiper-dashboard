ab07f79148e8bb61ac5f3e7ad1ebcee6ed96d2c5
Anand Sandilya <17026547+anand-san@users.noreply.github.com>
2024-10-20 01:12:42 +0200

    feat: add profile card

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,11 +1,47 @@
-import { useGetUsers } from "@/api/useUserQuery";
+import { useAuth } from "@/hooks/useAuth";
 
-export default function Profile() {
-  const { isPending, isError, data, error } = useGetUsers();
+const UserProfile = () => {
+  const { user, loading } = useAuth();
 
-  if (isPending) return <div>Loading...</div>;
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-  if (isError) return <div>Error: {error.message}</div>;
+  return (
+    user && (
+      <div className="shadow-lg rounded-lg p-6 max-w-md mx-auto border">
+        <div className="flex items-center space-x-4">
+          <img
+            src={user?.photoURL || "/user.svg"}
+            alt={user?.displayName || "Display name Placeholder"}
+            className="w-20 h-20 rounded-full object-cover"
+          />
+          <div>
+            <h2 className="text-2xl font-semibold ">{user.displayName}</h2>
+            <p className="">{user.email}</p>
+          </div>
+        </div>
+        <div className="mt-6 space-y-4">
+          <div className="flex justify-between">
+            <span className="">User ID:</span>
+            <span className="font-medium">{user.uid}</span>
+          </div>
+          <div className="flex justify-between">
+            <span className="">Joined:</span>
+            <span className="font-medium">
+              {user.metadata.creationTime ?? "N/A"}
+            </span>
+          </div>
+          <div className="flex justify-between">
+            <span className="">Last Sign In:</span>
+            <span className="font-medium">
+              {user.metadata.lastSignInTime ?? "N/A"}
+            </span>
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
 
-  return <div>{JSON.stringify(data)}</div>;
-}
+export default UserProfile;
